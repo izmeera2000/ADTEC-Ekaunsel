5d3134224a9ffcdbd2688d9a732dc5b7457d12a0
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2024-09-04 22:50:04 +0800

    sendmali
    sdsd

--- admin/server.php
+++ admin/server.php
@@ -11,7 +11,9 @@ require __DIR__ . '/../route.php';
 
 $site_url = $_ENV['site1'];
 // debug_to_console2($site_url);
-
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 // $username = "";
 // $email = "";
 // global $$errors;
@@ -829,6 +831,58 @@ if (isset($_POST['addsoalan'])) {
 if (isset($_POST['addsoalan'])) {
 removepic( $site_url ."assets/img/user/". $_SESSION['user_details']['id']/ "/" . $_SESSION['user_details']['image_url'] );
 uploadpic_id($_SESSION['user_details']['id'], $err);
+}
+
+function sendmail($receiver ,$title ,$message ="")
+{
+
+
+  //Load Composer's autoloader
+
+  //Create an instance; passing `true` enables exceptions
+  $mail = new PHPMailer(true);
+
+  try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host = 'kaunselingadtectaiping.com.my';                     //Set the SMTP server to send through
+    $mail->SMTPAuth = true;                                   //Enable SMTP authentication
+    $mail->Username = 'temujanji@kaunselingadtectaiping.com.my';                     //SMTP username
+    $mail->Password = 'temujanji@33';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('temujanji@fingerprint.kaunselingadtectaiping.com.my', 'Temu Janji');
+    $mail->addAddress($receiver);     //Add a recipient
+    // $mail->addAddress('ellen@example.com');               //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+
+    $mail->Subject = $title;
+    if (!$message){
+      
+  
+    $mail->Body = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+  }else{
+    $mail->Body = $message;
+    $mail->AltBody = $message;
+  }
+    $mail->send();
+    echo 'Message has been sent';
+  } catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+  }
 }
 
 ?>
