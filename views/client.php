94fa5ef75eabed9fad43be831e67fb248180f56b
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2024-09-15 02:17:30 +0800

    asdsa

--- views/client.php
+++ views/client.php
@@ -12,7 +12,7 @@
 
     <script>
         let ws;
-        const serverUrl = "wss://kaunselingadtectaiping.com.my/chat"; // Replace with your WebSocket server URL
+        const serverUrl = "ws://kaunselingadtectaiping.com.my/chat"; // Replace with your WebSocket server URL
         const reconnectInterval = 5000; // Time to wait before attempting reconnection (in milliseconds)
 
         function connectWebSocket() {
